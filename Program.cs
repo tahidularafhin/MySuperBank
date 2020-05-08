3b66eec5ab91ffb08f28cb7199b4a7064c3467ec
tahidularafhin <48841466+tahidularafhin@users.noreply.github.com>
2020-05-08 14:25:15 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Tahidul",10000);
+            Console.WriteLine($"Account {account.Number}   was created for {account.Owner} with {account.Balance}. " );
+
+            account.MakeWithdrawal(120, DateTime.Now, "Mazumder");
+            //Console.WriteLine(account.Balance);
+
+
+
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+            //Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
